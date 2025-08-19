25c2347e2f316642957d48391d86770229a49f36
kazama-ndlc <kazama-y@mss-2005.co.jp>
2025-08-19 16:01:46 +0900

    Add root endpoint

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/hello', (req, res) => {
-  res.send('こんにちは！');
+app.get('/', (req, res) => {
+  res.send('poc_notification_api is running!!!!!!');
 });
 
 app.listen(PORT, () => {
